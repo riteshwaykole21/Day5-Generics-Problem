79e7a784d9f506091e505039946517c7e95bd72b
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-07-06 08:50:56 +0530

    [Ritesh]add : Find Max Float Number

--- GenericsPracticeProblem/IntegerFindMax.cs
+++ GenericsPracticeProblem/IntegerFindMax.cs
@@ -23,5 +23,20 @@ namespace GenericsPracticeProblem
                 Console.WriteLine($"{Third} is Greater");
             }
         }
+        public void FindMaxValue(float First, float Second ,float Third)
+        {
+            if (First.CompareTo(Second) >= 0 && First.CompareTo(Third) >= 0)
+            {
+                Console.WriteLine($"{First} is Greater");
+            }
+            else if (Second.CompareTo(Third) >= 0 && Second.CompareTo(First) >= 0)
+            {
+                Console.WriteLine($"{Second} is Greater");
+            }
+            else if (Third.CompareTo(First) >= 0 && Third.CompareTo(Second) >= 0)
+            {
+                Console.WriteLine($"{Third} is Greater");
+            }
+        }
     }
 }
--- GenericsPracticeProblem/Program.cs
+++ GenericsPracticeProblem/Program.cs
@@ -12,6 +12,7 @@ namespace GenericsPracticeProblem
         {
             IntegerFindMax integerFindMax = new IntegerFindMax();
             integerFindMax.FindMaxValue(10,20,30);
+            integerFindMax.FindMaxValue(10.20f, 20.30f, 50.60f);
             Console.ReadLine();
         }
     }
