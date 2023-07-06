af04d8cb345a8a9d2849ba37afbe2ec0fb26018e
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-07-06 09:30:51 +0530

    [Ritesh]add : Using Generics Method

--- GenericsPracticeProblem/Program.cs
+++ GenericsPracticeProblem/Program.cs
@@ -10,10 +10,26 @@ namespace GenericsPracticeProblem
     {
         static void Main(string[] args)
         {
-            IntegerFindMax integerFindMax = new IntegerFindMax();
-            integerFindMax.FindMaxValue(10,20,30);
-            integerFindMax.FindMaxValue(10.20f, 20.30f, 50.60f);
-            integerFindMax.FindMaxValue("Ritesh","Rushikesh","Ronit");
+            Console.WriteLine("Welcome To Genrics Use CXases");
+            Console.WriteLine("1 : Find Max Interger Float And String");
+            Console.WriteLine("2 : Find Maximum Using Generics Method");
+            Console.WriteLine("Enter A Optiom");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch(Option)
+            {
+                case 1:
+                    IntegerFindMax integerFindMax = new IntegerFindMax();
+                    integerFindMax.FindMaxValue(10, 20, 30);
+                    integerFindMax.FindMaxValue(10.20f, 20.30f, 50.60f);
+                    integerFindMax.FindMaxValue("Ritesh", "Rushikesh", "Ronit");
+                    break;
+                case 2:
+                    Refactor1 refactor1 = new Refactor1();
+                    refactor1.FindMaxValue<int>(10, 20, 30);
+                    refactor1.FindMaxValue<float>(10.20f, 20.30f, 50.60f);
+                    refactor1.FindMaxValue<string>("Ritesh", "Rushikesh", "Ronit");
+                    break;
+            }
             Console.ReadLine();
         }
     }
